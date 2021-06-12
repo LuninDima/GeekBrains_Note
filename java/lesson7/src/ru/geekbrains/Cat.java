c32bd7b07fa4f39ddf5824797dae37ec740a1e39
LuninDima <moon-di-2009@yandex.ru>
2021-06-12 10:34:06 +0300

    1

---
+++ java/lesson7/src/ru/geekbrains/Cat.java
@@ -0,0 +1,18 @@
+package ru.geekbrains;
+
+public class Cat {
+    public String name;
+    public int appetite;
+    public boolean satiety;
+
+    public Cat(String name, int appetite, boolean satiety) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = satiety;
+    }
+
+    public void eat(Plate p) {
+            p.decreaseFood(appetite);
+    }
+}
+
---
+++ java/lesson8/src/ru/geekbrains/Treadmill.java
@@ -0,0 +1,8 @@
+package ru.geekbrains;
+
+public class Treadmill {
+    int treadMillDistance = 15;
+    public static void Running(String name, int distanceRun) {
+        Moving.Run(name, distanceRun);
+    }
+}
