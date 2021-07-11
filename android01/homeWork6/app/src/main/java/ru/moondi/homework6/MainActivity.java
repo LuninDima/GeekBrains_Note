d56a0dd88fc3545fbffa33429a04dddbab3612d1
LuninDima <moon-di-2009@yandex.ru>
2021-07-11 14:54:12 +0300

    1. инициализация меню в верхнем тулбаре. создание слушателя нажатий на кнопки меню.

--- android01/homeWork6/app/src/main/java/ru/moondi/homework6/MainActivity.java
+++ android01/homeWork6/app/src/main/java/ru/moondi/homework6/MainActivity.java
@@ -1,8 +1,14 @@
 package ru.moondi.homework6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +23,48 @@ public class MainActivity extends AppCompatActivity {
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+           @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(MainActivity.this, "Настройки", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_main:
+                Toast.makeText(MainActivity.this, "Главная", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_favorite:
+                Toast.makeText(MainActivity.this, "Избранное", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_search:
+                Toast.makeText(MainActivity.this, "Поиск", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+           @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+             @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+
+
+        });
+        return true;
     }
 }
