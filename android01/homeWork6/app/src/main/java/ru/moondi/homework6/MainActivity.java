156c1e1ecebd5a708a1d75eafb9c1a974586c622
LuninDima <moon-di-2009@yandex.ru>
2021-07-11 14:32:25 +0300

    1. создание метода inittoolbar() для инициализации Тулбара.

--- android01/homeWork6/app/src/main/java/ru/moondi/homework6/MainActivity.java
+++ android01/homeWork6/app/src/main/java/ru/moondi/homework6/MainActivity.java
@@ -3,6 +3,7 @@ package ru.moondi.homework6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
 
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
